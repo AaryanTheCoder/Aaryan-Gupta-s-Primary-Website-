f33e6d675bbee097c07b0f467ed63b46cb959d91
Aaryan Gupta <gupta156048@gapps.uwcsea.edu.sg>
2026-03-22 14:50:33 +0800

    Add files via upload

---
+++ Server2.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  console.log('Requested URL: ' + request.url);
+  console.log('Request Method: ' + request.method);
+
+  if (request.url === '/about' && request.method === 'GET') {
+        response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+        response.end('This is the about page. Welcome aboard! 🚢');
+  }
+  else if (request.url === '/contact' && request.method === 'GET') {
+    response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    response.write('Ahoy! Need to reach us? ');
+    response.end('Contact us at: contact@example.com.sg');
+
+  }
+  else if (request.url==='/ramen'&& request.method === 'GET'){
+    response.writeHead(200,{'Content-Type': 'text/plain; charset=utf-8'});
+    response.write('Welcome to Neon Noodles! ==============LATE NITE MENU=============\n');
+    response.write('RAMEN\n1. Quantum Truffle Ramen\n\nEXTRA TOPPINGS\n1. Hacktivist Pork\n2. Cybernetic Egg\n3. Glowing Scallion');
+    response.end();
+  }
+  else {
+    response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    response.end('Home page: try /about, /contact, or /ramen');
+  }
+
+});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, function () {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
