ecc9c9e71cda7fafd76111bea6301a51649330ed
Aaryan Gupta <aaryangupta.com@gmail.com>
2026-03-23 09:09:38 +0800

    update

--- Server2.js
+++ Server2.js
@@ -20,9 +20,62 @@ const server = http.createServer((request, response) => {
     response.write('RAMEN\n1. Quantum Truffle Ramen\n\nEXTRA TOPPINGS\n1. Hacktivist Pork\n2. Cybernetic Egg\n3. Glowing Scallion');
     response.end();
   }
+  else if (request.url==='/kaomoji'&& request.method === 'GET'){
+    response.end(`
+      
+<html>
+
+<head>
+    <title>Simple Kaomoji Table</title>
+</head>
+
+<h1>Happy Kaomoji Table</h1>
+
+<p>Click a kaomoji to copy it.</p>
+
+<table border="1">
+<tr>
+<th>Kaomoji</th>
+<th>Name</th>
+</tr>
+
+<tr>
+<td>
+<button onclick="copyKaomoji('(◕‿◕)')">(◕‿◕)</button>
+</td>
+<td>Simple happy</td>
+</tr>
+
+<script>
+function copyKaomoji(text)
+{
+navigator.clipboard.writeText(text);
+alert("Copied: " + text);
+}
+</script>
+
+<tr>
+<td><button onclick="copyKaomoji('٩(^‿^)۶')">٩(^‿^)۶</button></td>
+<td>Happy</td>
+</tr>
+
+<tr>
+<td><button onclick="copyKaomoji('(｡◕‿◕｡)')">(｡◕‿◕｡)</button></td>
+<td>Cute happy</td>
+</tr>
+
+<tr>
+<td><button onclick="copyKaomoji('(ﾉ◕ヮ◕)ﾉ')">(ﾉ◕ヮ◕)ﾉ</button></td>
+<td>Hands up</td>
+</tr>
+
+</table>
+
+</html>`);
+  }
   else {
     response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-    response.end('Home page: try /about, /contact, + 2 random hidden one! /n Made by Aaryan G');
+    response.end('Home page: try /about, /contact, + 2 random hidden one! \n Made by Aaryan G');
   }
 
 });
